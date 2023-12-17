c802fa39249411ef5f151d30ec510fb2d13a05e2
Dhananjay <ya695678@gmai.com>
2023-12-17 11:02:30 +0530

    hard problem solved

--- 03-Async-Javascript/hard (promises)/2-sleep-completely.js
+++ 03-Async-Javascript/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,16 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
+function sleep(seconds) {
+  const start = new Date().getTime();
+  let elapsedTime = 0;
 
+  while (elapsedTime < seconds) {
+    elapsedTime = new Date().getTime() - start;
+  }
 }
+
+// Example usage: Sleep for 3000 milliseconds (3 seconds)
+console.log("Start");
+sleep(3000);
+console.log("After sleeping");
