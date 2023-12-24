6144dd596e0b25be1bc6c4f6c4c9e08cc2313399
Dhananjay <ya695678@gmai.com>
2023-12-24 10:47:54 +0530

    mongo

--- 07-mongo/index.js
+++ 07-mongo/index.js
@@ -1,14 +1,87 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
-const adminRouter = require("./routes/admin")
-const userRouter = require("./routes/user");
 
-// Middleware for parsing request bodies
+
+mongoose.connect('your-mongodb-url');
+
+
+const AdminSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const CourseSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    price: Number,
+    image: String,
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+
 app.use(bodyParser.json());
-app.use("/admin", adminRouter)
-app.use("/user", userRouter)
 
-app.listen(3000, () => {
+
+app.post('/admin/signup', (req, res) => {
+    Admin.create({
+        username: req.body.username,
+        password: req.body.password
+    });
+    res.json({
+        message: 'Admin created successfully'
+    })
+});
+
+app.post('/admin/courses', (req, res) => {
+    Course
+    .create({
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.body.image
+    })
+});
+
+app.get('/admin/courses', (req, res) => {
+    Course
+        .find()
+        .then(courses => {
+            res.json(courses);
+        })
+});
+
+
+app.post('/users/signup', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    });
+    res.json({
+        message: 'User created successfully'
+    })
+});
+
+app.get('/users/courses', (req, res) => {
+    Course
+        .find()
+        .then(courses => {
+            res.json(courses);
+        })
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
