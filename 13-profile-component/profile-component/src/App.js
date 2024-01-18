6c0514019743d3796c5b644b6c80980041b947ab
Dhananjay <ya695678@gmai.com>
2024-01-18 21:47:42 +0530

    dynamic card

--- 13-profile-component/profile-component/src/App.js
+++ 13-profile-component/profile-component/src/App.js
@@ -2,7 +2,14 @@ import Card from "./Components/Card";
 function App() {
   return (
     <div className="App bg-blue-950	h-screen w-screen grid place-content-center">
-      <Card />
+      <Card 
+      imgUrl={"https://hips.hearstapps.com/hmg-prod/images/atv-selena-gomez-my-mind-and-me-key-art-1663335059.jpg?crop=1.00xw:0.745xh;0,0.0774xh&resize=1200:*"}
+      name={"Salena Gomez"}
+      place={"London"}
+      followers={"80k"}
+      likes={"806k"}
+      photos={"1.4k"}
+      />
     </div>
   );
 }
--- 13-profile-component/profile-component/src/Components/Profile.jsx
+++ 13-profile-component/profile-component/src/Components/Profile.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 
-const Profile = () => {
-  const imgClass =
-    'bg-[url("https://i.pinimg.com/originals/14/87/06/14870690c27adc50628dd18f87ca668d.jpg")]';
-
+const Profile = ({imgUrl,name,place}) => {
   return (
     <div className="w-full h-full grid place-content-center">
       <div className="flex justify-center">
-        <div
-          className={`bg-no-repeat h-28 w-28 ${imgClass} rounded-full bg-cover flex justify-center`}
-        ></div>
+        <img
+          className={`h-28 w-28 rounded-full flex justify-center`}
+          src={imgUrl}
+        ></img>
       </div>
 
       <div className="flex gap-3">
-        <h3 className="font-bold">Salena Gomez</h3>
+        <h3 className="font-bold">{name}</h3>
         <h6 className="font-extralight">31</h6>
       </div>
 
-      <h6 className="font-extralight flex justify-center ">London</h6>
+      <h6 className="font-extralight flex justify-center ">{place}</h6>
     </div>
   );
 };
